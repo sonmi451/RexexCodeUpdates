e86d27356fe31ae98907927295af50deb19c0456
Aisha McLean <aishamclean451@gmail.com>
2018-02-05 17:18:56 +0000

    end of day commit

--- Regex.py
+++ Regex.py
@@ -10,6 +10,7 @@ startBrace, countup = 0, 0
 with open('TestUpdate.txt', 'w') as out:
     with open('Test.txt') as readIn:
         for line in readIn:
+            #new string
             if re.search(regexSetValue, line):
                 SetValueToEdit = re.search(regexSetValue, line)
                 replace = str("getStringEdit(\""+ SetValueToEdit.group(3) +"\")")
@@ -17,16 +18,17 @@ with open('TestUpdate.txt', 'w') as out:
                 print("Found old form adaptor " + replace)
                 print("replace with new method ",newMethod)
                 out.write(newMethod)
+            #Grid
             if re.search(regexRowInGrid, line):
                 RowInGridToEdit = re.search(regexRowInGrid, line)
-                replace = str("getStringEdit(\""+ RowInGridToEdit.group(3) +"\")")
+                replace = str("getGrid(\""+ RowInGridToEdit.group(3) +"\")")
                 newMethod = (RowInGridToEdit.group(1) + replace + RowInGridToEdit.group(4))
                 print("Found old form adaptor " + replace)
                 print("replace with new method ",newMethod)
                 out.write(newMethod)
             #Tab activate
-            if re.search(regexClickButton, line):
-                TabActivateToEdit = re.search(regexClickButton, line)
+            if re.search(regexTabActivate , line):
+                TabActivateToEdit = re.search(regexTabActivate, line)
                 replace = str("getTabPage(\""+ TabActivateToEdit.group(3) +"\")")
                 newMethod = (TabActivateToEdit.group(1) + replace + TabActivateToEdit.group(4))
                 print("Found old form adaptor " + replace)
